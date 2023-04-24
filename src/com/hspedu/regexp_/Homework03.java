9b694b10740f6bf0ed20cdbb38a160099d642eb8
Hao Zhou <v-zhhao@microsoft.com>
2023-04-23 20:49:26 -0700

    initial

--- src/com/hspedu/regexp_/Homework03.java
+++ src/com/hspedu/regexp_/Homework03.java
@@ -6,12 +6,16 @@ import java.util.regex.Pattern;
 public class Homework03 {
     public static void main(String[] args) {
         String content = "http://www.sohu.com:8080/abc/index.html";
-        String regStr = "^[+-]?(0|([1-9])\\d*)(\\.\\d+)?$";
+        String regStr = "^([a-zA-Z]+)://([\\w.]+):(\\d+)/\\w+/(\\w+\\.\\w+)$";
         Pattern compile = Pattern.compile(regStr);
         Matcher matcher = compile.matcher(content);
 
         while (matcher.find()) {
             System.out.println(matcher.group(0));
+            System.out.println(matcher.group(1));
+            System.out.println(matcher.group(2));
+            System.out.println(matcher.group(3));
+            System.out.println(matcher.group(4));
         }
     }
 }
